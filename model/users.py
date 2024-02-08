7d0b8135072492a6823f81621fa03f89f59b0240
varunm532 <varundeep.pillai@gmail.com>
2024-02-08 01:36:57 -0800

    stocks database structure in model.users

--- model/users.py
+++ model/users.py
@@ -68,6 +68,92 @@ class Post(db.Model):
 # -- a.) db.Model is like an inner layer of the onion in ORM
 # -- b.) User represents data we want to store, something that is built on db.Model
 # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL
+class Stocks(db.Model):
+    _tablename_ = 'stocks'
+    
+    # define the stock schema with "vars" from object
+    id = db.Column(db.Integer, primary_key=True)
+    _symbol = db.Column(db.String(255),unique=False,nullable=False)
+    _company = db.Column(db.String(255),unique=False,nullable=False)
+    _quantity = db.Column(db.String(255),unique=False,nullable=False)
+    
+    # constructor of a User object, initializes the instance variables within object (self)
+    def _init_(self,symbol,company,quantity):
+        self._symbol = symbol
+        self._company = company
+        self._quantity = quantity
+# symbol
+    @property
+    def symbol(self):
+        return self._symbol
+    
+    @symbol.setter
+    def symbol(self,symbol):
+        self._symbol = symbol
+#company
+    @property
+    def company(self):
+        return self._company
+    
+    @company.setter
+    def company(self,company):
+        self._company = company
+#quantity
+    @property
+    def quantity(self):
+        return self._quantity
+    
+    @quantity.setter
+    def quantity(self,quantity):
+        self._quantity = quantity
+    
+    # output content using str(object) in human readable form, uses getter
+    # output content using json dumps, this is ready for API response
+    def __str__(self):
+        return json.dumps(self.read())
+    
+    # CRUD create/add a new record to the table
+    # returns self or None on error
+    def create(self):
+        try:
+            # creates a person object from User(db.Model) class, passes initializers
+            db.session.add(self)  # add prepares to persist person object to Users table
+            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
+            return self
+        except IntegrityError:
+            db.session.remove()
+            return None
+        
+     # CRUD update: updates user name, password, phone
+    # returns self
+    def update(self,symbol="",company="",quantity=""):
+        """only updates values with length"""
+        if len(symbol) > 0:
+            self.symbol = symbol
+        if len(company) > 0:
+            self.company = company
+        if len(quantity) > 0:
+            self.quantity = quantity
+        db.session.commit()
+        return self
+    
+    # CRUD read converts self to dictionary
+    # returns dictionary
+    def read(self):
+        return {
+            "id": self.id,
+            "symbol": self.symbol,
+            "company": self.company,
+            "quantity": self.quantity,
+        }
+    # Builds working data for testing
+    def initUsers():
+        with app.app_context():
+            """Create database and tables"""
+            db.create_all()
+
+        
+
 class User(db.Model):
     __tablename__ = 'users'  # table name is plural, class name is singular
 
